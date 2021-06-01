b7993ada717464ddcd2af391b67f3524be1be92e
trashdeal <trashdeal2021@gmail.com>
2021-06-01 20:14:50 +0530

    Brendon; added toggle button on use bin page

--- app/src/main/java/com/example/trashdeal/SplashScreen.kt
+++ app/src/main/java/com/example/trashdeal/SplashScreen.kt
@@ -5,7 +5,6 @@ import android.media.MediaPlayer
 import android.net.Uri
 import android.os.Bundle
 import android.view.WindowManager
-import android.widget.ImageView
 import android.widget.VideoView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -17,8 +16,6 @@ class SplashScreen : AppCompatActivity() {
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
-        val bgnd_img = findViewById<ImageView>(R.id.backimg)
-
         setContentView(R.layout.activity_splash_screen)
         val videoView = findViewById<VideoView>(R.id.videoView)
         val video = Uri.parse("android.resource://" + packageName + "/" + R.raw.logosplashscreen)
@@ -28,8 +25,5 @@ class SplashScreen : AppCompatActivity() {
             finish()
         })
         videoView.start()
-
-
     }
-
 }
