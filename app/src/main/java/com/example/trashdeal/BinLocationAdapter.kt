b056a8f1de6da00107cf5145e30890d8a7ba988c
trashdeal <trashdeal2021@gmail.com>
2021-06-03 16:13:00 +0530

    Brendon; fixing tabitha's issue

--- app/src/main/java/com/example/trashdeal/BinLocationAdapter.kt
+++ app/src/main/java/com/example/trashdeal/BinLocationAdapter.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
-import android.widget.Button
 import android.widget.TextView
 import java.util.*
 
@@ -34,16 +33,9 @@ class BinLocationAdapter(private val context: Context,
         val binName = rowView.findViewById<TextView>(R.id.binName)
         val binAddress = rowView.findViewById<TextView>(R.id.binAddress)
         val binDistance = rowView.findViewById<TextView>(R.id.binDistance)
-        val viewBtn = rowView.findViewById<Button>(R.id.goToBinBtn)
         binName.text = dataSource[position].binName
         binAddress.text = dataSource[position].binAddress
         binDistance.text = dataSource[position].binDistance+"km away"
-        viewBtn.setOnClickListener{
-//            Log.i("TAG", "Tapped on ${dataSource[position].binID}")
-//            startActivity(Intent(context, ConnectBin::class.java).apply {
-//                putExtra("userBin", dataSource[position].binID)
-//            })
-        }
         return rowView
     }
 }
